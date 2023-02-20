1ff06f16860cef1cb6885f7446788fec154e1287
Constantin <konstantindanilov19@hotmail.com>
2023-02-20 20:38:19 +0200

    moved all of the loading and making the get posts requestlogic into a hook

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { PostList } from './components/PostList';
 import { Button } from './components/UI/Button/Button';
 import { Loader } from './components/UI/Loader/Loader';
 import { Modal } from './components/UI/Modal/Modal';
+import { useFetching } from './hooks/useFetching';
 import { usePosts } from './hooks/usePosts';
 import './scss/app.scss';
 import { IPost } from './types/posts';
@@ -15,20 +16,14 @@ const App = () => {
   const [filter, setFilter] = useState({ sort: '', search: '' });
   const [isModalVisible, setIsModalVisible] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.search);
-  const [isLoading, setIsLoading] = useState(false);
+  
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const resp = await postsAPI.getPosts();
+    setPosts(resp.data);
+  })
 
   useEffect(() => {
-    (async () => {
-      setIsLoading(true);
-      try {
-        const resp = await postsAPI.getPosts();
-        setPosts(resp.data);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setIsLoading(false);
-      }
-    })();
+    fetchPosts()
   }, []);
 
   const createNewPost = (newPost: IPost) => {
@@ -53,7 +48,10 @@ const App = () => {
       </Modal>
       <hr style={{ margin: '15px 0' }} />
       <PostFilter filter={filter} setFilter={setFilter} />
-      {isLoading ? (
+      {
+        postError && <h1><>There has been an error `${postError}`</></h1>
+      }
+      {isPostsLoading ? (
         <div style={{ display: 'flex', justifyContent: 'center' }}>
           <Loader />
         </div>
