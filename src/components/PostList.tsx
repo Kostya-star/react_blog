456a4cc0cb061f6af3c73d2848c329a3c7aa9c91
Constantin <konstantindanilov19@hotmail.com>
2023-02-20 16:35:41 +0200

    implemented some animation

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react';
 import { IPost } from '../types/posts';
 import { PostItem } from './Post/PostItem';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import '../scss/app.scss';
 
 interface IPostListProps {
   title: string;
@@ -12,18 +14,17 @@ export const PostList: FC<IPostListProps> = ({ title, posts, deletePost }) => {
   return (
     <div>
       <h1>{title}</h1>
-      {posts.length ? (
-        posts.map((post, index) => (
-          <PostItem
-            id={index + 1}
-            key={post.id}
-            post={post}
-            deletePost={deletePost}
-          />
-        ))
-      ) : (
-        <h1 style={{ textAlign: 'center' }}>No posts</h1>
-      )}
+      <TransitionGroup>
+        {posts.length ? (
+          posts.map((post, index) => (
+            <CSSTransition key={post.id} timeout={500} classNames="post">
+              <PostItem id={index + 1} post={post} deletePost={deletePost} />
+            </CSSTransition>
+          ))
+        ) : (
+          <h1 style={{ textAlign: 'center' }}>No posts</h1>
+        )}
+      </TransitionGroup>
     </div>
   );
 };
