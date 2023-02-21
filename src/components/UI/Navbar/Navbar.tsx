2f8ae60bd7f71b340907c1903df59bfddb97650d
Constantin <konstantindanilov19@hotmail.com>
2023-02-21 21:02:18 +0200

    made navbar fixed

--- src/components/UI/Navbar/Navbar.tsx
+++ src/components/UI/Navbar/Navbar.tsx
@@ -1,29 +1,33 @@
 import { Link, useNavigate } from 'react-router-dom';
 import { Button } from '../Button/Button';
 import s from './Navbar.module.scss';
-import { useContext } from 'react'
+import { useContext } from 'react';
 import { AuthContext } from '../../../context/context';
 import { IsAuthContext } from '../../../types/context';
 
 export const Navbar = () => {
-  const { isAuth, setIsAuth } = useContext(AuthContext) as IsAuthContext
-  const navigate = useNavigate()
+  const { isAuth, setIsAuth } = useContext(AuthContext) as IsAuthContext;
+  const navigate = useNavigate();
 
   const onLogout = () => {
-    setIsAuth(false)
-    localStorage.removeItem('isAuth')
-    navigate('/login')
-  }
+    setIsAuth(false);
+    localStorage.removeItem('isAuth');
+    navigate('/login');
+  };
 
   return (
-    <nav className={s.navbar}>
-      <div>
-        <Link to="/posts">Posts</Link>
-        <Link to="/about">About</Link>
-      </div>
-      <div>
-        <Button onClick={onLogout}>Log out</Button>
-      </div>
-    </nav>
+    <>
+
+      <nav className={s.navbar}>
+        <div>
+          <Link to="/posts">Posts</Link>
+          <Link to="/about">About</Link>
+        </div>
+        <div>
+          <Button onClick={onLogout}>Log out</Button>
+        </div>
+      </nav>
+      <div style={{ height: '50px' }}>NAVBAR</div>
+    </>
   );
 };
